70eaa879f0388c139b4755ecc2edcec8a6777f34
@michael <michael@enhance.com>
2019-11-08 01:16:20 +0000

    Cleaning up test suite.

--- test/memcell_spec.erl
+++ test/memcell_spec.erl
@@ -2,11 +2,7 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--define(PRINT(Var), io:format("DEBUG: ~p:~p - ~p~n~n ~p~n~n", [?MODULE, ?LINE, ??Var, Var])).
-
--define(DEFAULT, first).
-
-get_empty_memcell() ->
+get_empty_memcell_test() ->
     Capacity = 2,
     N = 1,
     {ok, Pid} = memcell:start(Capacity),
@@ -15,46 +11,45 @@ get_empty_memcell() ->
 get_no_elements_test() ->
     Capacity = 2,
     N = 0,
-    {ok, Pid} = memcell:start(Capacity, [?DEFAULT]),
+    {ok, Pid} = memcell:start(Capacity, [first]),
     {N, []} = memcell:get(N, Pid).
 
 get_one_random_element_test() ->
     Capacity = 2,
     N = 1,
-    {ok, Pid} = memcell:start(Capacity, [?DEFAULT]),
-    {N, [?DEFAULT | Tail]} = memcell:get(N, Pid),
+    {ok, Pid} = memcell:start(Capacity, [first]),
+    {N, [first | Tail]} = memcell:get(N, Pid),
     ?assertEqual(N - 1, length(Tail)).
 
 get_two_random_elements_test() ->
     Capacity = 10,
     N = 2,
-    {ok, Pid} = memcell:start(Capacity, [?DEFAULT]),
-    {N, [?DEFAULT | Tail]} = memcell:get(N, Pid),
+    {ok, Pid} = memcell:start(Capacity, [first]),
+    {N, [first | Tail]} = memcell:get(N, Pid),
     ?assertEqual(N - 1, length(Tail)).
 
 get_capacity_test() ->
     Capacity = 10,
     N = Capacity,
-    {ok, Pid} = memcell:start(Capacity, [?DEFAULT]),
-    {N, [?DEFAULT | Tail]} = memcell:get(N, Pid),
+    {ok, Pid} = memcell:start(Capacity, [first]),
+    {N, [first | Tail]} = memcell:get(N, Pid),
     ?assertEqual(N - 1, length(Tail)).
 
 get_more_than_capacity_test() ->
     Capacity = 10,
     N = 2 * Capacity,
-    {ok, Pid} = memcell:start(Capacity, [?DEFAULT]),
-    {N, [?DEFAULT | Tail]} = memcell:get(N, Pid),
+    {ok, Pid} = memcell:start(Capacity, [first]),
+    {N, [first | Tail]} = memcell:get(N, Pid),
     ?assertEqual(N - 1, length(Tail)).
 
 insert_one_element_test() ->
     Capacity = 2,
     Insert = second,
-    {ok, Pid} = memcell:start(Capacity, [?DEFAULT]),
+    {ok, Pid} = memcell:start(Capacity, [first]),
     ok = memcell:insert(Insert, Pid),
     {Capacity, Response} = memcell:get(Capacity, Pid),
-    ?PRINT(Response),
     HasNew = lists:any(fun(El) -> El == Insert end, Response),
-    HasOld = lists:any(fun(El) -> El == ?DEFAULT end, Response),
+    HasOld = lists:any(fun(El) -> El == first end, Response),
     ?assertEqual(true, HasNew),
     ?assertEqual(true, HasOld).
 
@@ -63,12 +58,12 @@ insert_over_capacity_test() ->
     Second = second,
     Third = third,
     Forth = forth,
-    {ok, Pid} = memcell:start(Capacity, [?DEFAULT]),
+    {ok, Pid} = memcell:start(Capacity, [first]),
     ok = memcell:insert(Second, Pid),
     ok = memcell:insert(Third, Pid),
     ok = memcell:insert(Forth, Pid),
     {Capacity, Response} = memcell:get(Capacity, Pid),
-    HasFirst = lists:any(fun(El) -> El == ?DEFAULT end, Response),
+    HasFirst = lists:any(fun(El) -> El == first end, Response),
     HasSecond = lists:any(fun(El) -> El == Second end, Response),
     HasThird = lists:any(fun(El) -> El == Third end, Response),
     HasForth = lists:any(fun(El) -> El == Forth end, Response),
